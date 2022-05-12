c37a77263cd8443c839ebc9b5da38346b7b0e16f
jimmy <zhaofeng@shuinfo.com>
2022-05-12 17:03:02 +0800

    fix bug

--- lib/life_cycle_state.dart
+++ lib/life_cycle_state.dart
@@ -176,8 +176,10 @@ class LifecycleManager with WidgetsBindingObserver, RouteAware {
 
   initRoute(BuildContext context) {
     if (ModalRoute.of(context) != null) {
-      var route = ModalRoute.of(context) as PageRoute<dynamic>;
-      routeObserver.subscribe(this, route);
+      var route = ModalRoute.of(context) as PageRoute<dynamic>?;
+      if (route != null) {
+        routeObserver.subscribe(this, route);
+      }
     }
   }
 
